6b9a497358f8d6eb2f28f7babe5468e3948a834a
Sajal Kumar <sajalkumar.gd4@gmail.com>
2023-12-25 03:42:17 +0530

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/App.js 	new file:   frontend/src/components/Footer.jsx 	new file:   frontend/src/components/Header.jsx 	new file:   frontend/src/components/Product.jsx 	new file:   frontend/src/components/Rating.jsx 	modified:   frontend/src/index.css 	modified:   frontend/src/index.js 	new file:   frontend/src/screens/HomeScreen.jsx

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+
+const header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="sm" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>E-Commerse</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link to="/cart">
+                  <FaShoppingCart />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
+                  Sign in
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default header;
